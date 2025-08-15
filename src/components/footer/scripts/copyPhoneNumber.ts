1db24d14e606f05f6112e8a81233adfca14515bb
Kairo Chácara <144383548+ChacaraKairo@users.noreply.github.com>
2025-08-15 12:35:55 -0300

    Atualizar o copyPhoneNumber.ts

--- src/components/footer/scripts/copyPhoneNumber.ts
+++ src/components/footer/scripts/copyPhoneNumber.ts
@@ -1,7 +1,7 @@
 export function copyPhoneNumber(
   setShowAlert: (value: boolean) => void,
 ) {
-  navigator.clipboard.writeText('+5549989233265');
+  navigator.clipboard.writeText('+5519986011419');
   setShowAlert(true);
   setTimeout(() => setShowAlert(false), 3000);
 }
